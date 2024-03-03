f314a7b3b522211e9c4de7634f431c625e9a2413
KoifishVEVO <farhan210604@gmail.com>
2024-03-03 17:55:52 +0700

    tambahan fitur GUI

---
+++ TP_MOD_03_GUI/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TP_MOD_03_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button
+            // 
+            this.Button.Location = new System.Drawing.Point(508, 49);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(280, 88);
+            this.Button.TabIndex = 0;
+            this.Button.Text = "Submit";
+            this.Button.UseVisualStyleBackColor = true;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(74, 84);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(200, 20);
+            this.TextBox.TabIndex = 1;
+            this.TextBox.Text = "Masukkan Nama";
+            this.TextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Output
+            // 
+            this.Output.AutoSize = true;
+            this.Output.Location = new System.Drawing.Point(295, 297);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(39, 13);
+            this.Output.TabIndex = 2;
+            this.Output.Text = "Output";
+            this.Output.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Label Output;
+    }
+}
+
---
+++ TP_MOD_03_GUI/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_MOD_03_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            //Ini adalah event saat button di klik
+            //program mengambil teks yang diinput di textbox
+            string text = TextBox.Text;
+
+            //outputtan label yang bernama Output setelah button di klik
+            Output.Text = "Halo " + text;
+        }
+    }
+}
